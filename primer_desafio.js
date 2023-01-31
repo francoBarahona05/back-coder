7530d71c87c8dee162c47d1e1e3801ba0b174338
franco barahona <franco.barahona.07@gmail.com>
2023-01-30 23:24:31 -0300

    empeze el segundo desafio pero no pude solucionar el error , me dice require is not defined , y nunca pude probar como iba el codigo por que no me funcionaba el require , cuando lo llame exactamente igual que el profesor en clase , lo soluciono ni bien encuentre la manera de sacar ese error y poder probar el codigo , agradeceria un consejo. gracias por la paciencia

--- primer_desafio.js
+++ primer_desafio.js
@@ -1,31 +1,27 @@
+const fs = require("fs");
 
-class ticketManager {
-   constructor(){
-      this.events = [
-      ];
+let products = [];
+class productManager{
+   constructor(productName){
+      this.productName = productName
    }
-   getEvent = ()=>{
-      return this.events
-   }
-
-   addEvent = (title,descriptions,price,thumbnail,code,stock)=>{
-      if (this.events.find(elem => elem.code === code)) {
-         throw new error("el producto ya existe");
-      }
-      this.events.push({
-         id: (Math.random()*100).toString(36).slice(3),
-         title,
-         descriptions,
-         price,
-         thumbnail,
-         code,
-         stock
+   createProduct = async(title,description,price,thumbnail)=>{
+      products.push({
+         id : (Math.random()*100).toString(36).slice(9) ,
+         title , 
+         description,
+         price, 
+         thumbnail
       })
-   }
 
+      fs.writeFileSync(this.filename, JSON.stringify(products, null , 2))
+   }
 }
 
-const ticketManagerr = new ticketManager()
-ticketManagerr.addEvent(`smart tv`, `smart tv de 50 pulgadas marca samsung`, 500 ,`https://http2.mlstatic.com/D_NQ_NP_887081-MLA42141360831_062020-O.jpg`, `101010`,100)
+ async function desafio(){
+   const product = new productManager(`product.JSON`);
+   await product.createProduct(`funko maria`,`funko de coleccion`,500,`https//www.funko.coleccion.com`)
 
-console.log(ticketManagerr.getEvent())
+ }
+ desafio()
+// const productoUno = new productManager()
